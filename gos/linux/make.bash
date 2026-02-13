7b9d4228885fa4727c60ec6d19f87094427a89b0
Andrew Snyder <ajs@dancingjars.com>
2026-02-12 22:58:12 -0500

    gos/linux compiles with warning: relocation against "_ZNSt12_Vector_baseI12GlopKeyEventSaIS0_EE13_M_deallocateEPS0_j" in read-only section

--- gos/linux/make.bash
+++ gos/linux/make.bash
@@ -1,5 +1,5 @@
 g++ -o glop.o -m32 -c -Iinclude glop.cpp
-g++ -m32 -shared -dynamiclib -W1 -o libglop.so glop.o
+g++ -m32 -shared -dynamiclib -Wp,1 -o libglop.so glop.o
 rm -f glop.o
 mkdir -p lib
 mv libglop.so lib
